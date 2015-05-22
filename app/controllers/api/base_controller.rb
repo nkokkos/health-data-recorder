5a7f7a421396c7fcdfca84d1721f3b904c9d64cc
Nick Kokkos <nkokkos@gmail.com>
2015-05-22 08:55:17 +0000

    modified api controllers

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,29 +1,41 @@
-class Api::BaseController < ActionController::Base
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+class Api::BaseController < ActionController::Base
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  protect_from_forgery with: :null_session
+  before_action :destroy_session
   before_filter :authenticate_user_with_access_key!
   respond_to    :json
 
-   private
+    def authenticate_user_with_access_key!
 
-  def authenticate_user_with_access_key!
-	#puts "request.content_type: " + request.content_type
-	  if request.get? || request.post? && request.headers["Authorization"] &&
-      request.content_type == "application/json" &&
-	    User.find_by_access_id(request.headers["Authorization"])
-	      @current_user = User.find_by_access_id(request.headers["Authorization"])
-    else
-	    respond_to do |format|
-		  format.json { render json:  {:result => "Fail",
-								       :message => "Token is invalid or User was not found"
-									  }.to_json }
-		end
-  end #if
-	end
+        puts "request.content_type: " + request.content_type
 
-	def current_user_from_api_access
-	  @current_user
+          if request.get? || request.post? &&
+             request.headers["Authorization"] &&
+             request.content_type == "application/json" &&
+             User.find_by_access_id(request.headers["Authorization"])
+
+            @current_user = User.find_by_access_id(request.headers["Authorization"])
+          else
+            respond_to do |format|
+                  format.json { render json:  {:result => "Fail",
+                                                                       :message => "Token is invalid or User was not found"
+                                                                          }.to_json }
+                end
+          end
+        end
+
+        def current_user_from_api_access
+          @current_user
+        end
+
+ def destroy_session
+    request.session_options[:skip] = true
   end
+
+
+  private
 
   def record_not_found
     render json:  {:result => "Fail", :message => "User was not found. Abort"}.to_json
--- app/controllers/api/measures_controller.rb
+++ app/controllers/api/measures_controller.rb
@@ -1,13 +1,18 @@
-class Api::MeasuresController < Api::BaseController
+class Api:MeasuresController < Api::BaseController
   # responds to json or html requests
   # through inheritance from BaseController:
   def index
-    #respond_with
     if current_user_from_api_access
       respond_to do |format|
         format.json { render json:  Measure.all.to_json }
-	      format.html { render text:  Measure.all.to_json }
+        format.html { render text:  Measure.all.to_json  }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: { "result" => "Fail", "message" =>  "Invalid user"  }.to_json }
+        #format.html { render json: { "result" => "Fail","message"  =>  "Invalid user"  }.to_json }
       end
     end
   end
 end
+
