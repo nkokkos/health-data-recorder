dc86423b2240ae52552331602a23db42c361c823
Nick Kokkos <nkokkos@gmail.com>
2015-07-15 13:24:56 +0000

    worked on data trigger

--- app/controllers/triggers_controller.rb
+++ app/controllers/triggers_controller.rb
@@ -1,4 +1,5 @@
 class TriggersController < ApplicationController
+  before_filter :load_current_user
   
   def show
   end
@@ -6,10 +7,32 @@ class TriggersController < ApplicationController
   def index
     @user = User.find(current_user.id)
 	@trigger_block = TriggerBlock.where(:id => params[:trigger_block_id])
-	@triggers = [1,2,3]
+	@triggers = @trigger_block.first.triggers
 	@patient = User.where(:id => params[:patient_id])
   end
 
   def edit
   end
+  
+  def new
+    @user = User.find(current_user)
+    @trigger_block  = TriggerBlock.where(:id => params[:trigger_block_id])
+	@trigger = @trigger_block.first.triggers.build
+  end
+  
+  def create
+    @trigger_block  = TriggerBlock.where(:id => params[:trigger_block_id])
+    @trigger = @trigger_block.triggers.create(trigger_params)
+	redirect_to patient_trigger_block_triggers_path 
+  end
+  
+ private
+  def trigger_params
+     params.require(:trigger).permit(:device_id, :measure_id)
+  end
+  
+  def load_current_user
+    @user = User.find(current_user.id)
+  end
+
 end
